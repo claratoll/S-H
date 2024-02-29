3b02728325a48fe3399d26c49b4318cf2d08c446
claratoll <clara.toll@gmail.com>
2024-02-29 09:10:51 +0100

    workoutreg

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import useUser from '../features/useUser';
 import useData from '../features/useData';
@@ -9,20 +9,14 @@ const Workout = () => {
   const location = useLocation();
   const { workout, programId } = location.state;
   const user = useUser();
-  const { updateData } = useData();
+  const { getData, updateData } = useData();
   const [showInfo, setShowInfo] = useState(
     Array(workout.exercises.length).fill(false)
   );
   const [savedToFirebase, setSavedToFirebase] = useState('');
   const [userWorkoutInfo, setUserWorkoutInfo] = useState('');
-  const [weightsValues, setWeightsValues] = useState(
-    workout.exercises.map((exercise) => Array(exercise.sets).fill(0))
-  );
-  const [repsValues, setRepsValues] = useState(
-    workout.exercises.map((exercise) =>
-      Array(exercise.sets).fill(exercise.repetitions)
-    )
-  );
+  const [weightsValues, setWeightsValues] = useState([]);
+  const [repsValues, setRepsValues] = useState([]);
 
   const handleInfoClick = (index) => {
     const newShowInfo = [...showInfo];
@@ -30,7 +24,7 @@ const Workout = () => {
     setShowInfo(newShowInfo);
   };
 
-  const handleRepsChange = (exerciseIndex, setIndex, event) => {
+  const handleInputChange = (exerciseIndex, setIndex, event) => {
     const { name, value } = event.target;
     const newValue = Number(value);
 
@@ -64,6 +58,73 @@ const Workout = () => {
       console.error('Fel vid uppdatering av data:', error);
     }
   };
+
+  useEffect(() => {
+    const getFbWorkoutData = async () => {
+      try {
+        const data = await getData(programId.toString(), workout.id.toString());
+
+        if (data) {
+          setUserWorkoutInfo(
+            'Du har redan kört det här träningspasset en gång och du ser dina tidigare vikter och repetitioner nedan. Vill du köra det igen går det bra såklart, då uppdateras bara din gamla registrering med din nya.'
+          );
+          const exercisesData = Object.values(data);
+          console.log('data is:', data);
+
+          // För varje övning, hämta repetitions- och viktvärden
+          const repsValuesArray = workout.exercises.map((exercise, index) => {
+            // Kontrollera om det finns data för den här övningen
+            const exerciseData = exercisesData.find(
+              (data) => data.exerciseID === exercise.id
+            );
+
+            // Om det finns data, använd den, annars använd defaultvärden
+            if (exerciseData) {
+              return exerciseData.repetitions.map((rep) => Number(rep));
+            } else {
+              return Array(exercise.repetitions.length).fill(0);
+            }
+          });
+
+          const weightsValuesArray = workout.exercises.map(
+            (exercise, index) => {
+              const exerciseData = exercisesData.find(
+                (data) => data.exerciseID === exercise.id
+              );
+
+              if (exerciseData && exerciseData.weights) {
+                return exerciseData.weights.map((weight) => Number(weight));
+              } else {
+                return Array(exercise.weights.length).fill(0);
+              }
+            }
+          );
+
+          setRepsValues(repsValuesArray);
+          setWeightsValues(weightsValuesArray);
+        } else {
+          const exerciseRepsValues = workout.exercises.map((exercise) =>
+            Array(exercise.repetitions.length)
+              .fill(0)
+              .map((_, setIndex) => Number(exercise.repetitions[setIndex]))
+          );
+          const exerciseWeightsValues = workout.exercises.map((exercise) =>
+            exercise.weights && exercise.weights.length > 0
+              ? Array(exercise.weights.length)
+                  .fill(0)
+                  .map((_, setIndex) => Number(exercise.weights[setIndex]))
+              : []
+          );
+          setRepsValues(exerciseRepsValues);
+          setWeightsValues(exerciseWeightsValues);
+        }
+      } catch (error) {
+        console.error('Fel vid hämtning av data:', error);
+      }
+    };
+
+    getFbWorkoutData();
+  }, []);
 
   return (
     <div>
@@ -101,26 +162,44 @@ const Workout = () => {
                   <p>
                     Antal repetitioner:
                     <br />
-                    {[...Array(exercise.sets)].map((_, setIndex) => (
+                    {repsValues[index].map((reps, setIndex) => (
                       <React.Fragment key={setIndex}>
                         <input
                           type='number'
+                          key={`reps_${exercise.id}_${setIndex}`}
                           name={`reps_${exercise.id}_${setIndex}`}
-                          value={repsValues[index][setIndex] || ''}
+                          value={
+                            repsValues[index] && repsValues[index][setIndex]
+                              ? repsValues[index][setIndex] || ''
+                              : ''
+                          }
                           onChange={(event) =>
-                            handleRepsChange(index, setIndex, event)
+                            handleInputChange(index, setIndex, event)
                           }
                         />{' '}
-                        x{' '}
-                        <input
-                          type='number'
-                          name={`weight_${exercise.id}_${setIndex}`}
-                          value={weightsValues[index][setIndex] || ''}
-                          onChange={(event) =>
-                            handleRepsChange(index, setIndex, event)
-                          }
-                        />{' '}
-                        kg
+                        {weightsValues[index] &&
+                          !weightsValues[index].every(
+                            (weight) => weight === 0
+                          ) && (
+                            <React.Fragment>
+                              x{' '}
+                              <input
+                                type='number'
+                                key={`weight_${exercise.id}_${setIndex}`}
+                                name={`weight_${exercise.id}_${setIndex}`}
+                                value={
+                                  weightsValues[index] &&
+                                  weightsValues[index][setIndex]
+                                    ? weightsValues[index][setIndex] || ''
+                                    : ''
+                                }
+                                onChange={(event) =>
+                                  handleInputChange(index, setIndex, event)
+                                }
+                              />{' '}
+                              kg
+                            </React.Fragment>
+                          )}
                         <br />
                       </React.Fragment>
                     ))}
